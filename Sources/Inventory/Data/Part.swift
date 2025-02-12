58c1b7f425a2d241c2fe2534c8efbc715d07b866
Asif Ashadullah <156141923+asha0012-asif@users.noreply.github.com>
2025-02-11 20:45:03 -0500

    feat(Part): create Part data model

---
+++ Sources/Inventory/Data/Part.swift
@@ -0,0 +1,33 @@
+//
+//  Part.swift
+//  Inventory
+//
+//  Created by Asif Ashadullah on 2025-02-11.
+//
+
+import Foundation
+import Hummingbird
+
+struct Part {
+    let id: UUID
+    let name: String
+    let category: PartCategory
+    let size: Dimensions
+    let weight: Double
+}
+
+enum PartCategory {
+    case engine
+    case transmission
+    case brakes
+    case body
+}
+
+struct Dimensions {
+    let height: Double
+    let width: Double
+    let length: Double
+}
+
+// Every data model you create MUST contain this line (DON'T forget to change the object name)
+extension Part: ResponseEncodable, Decodable, Equatable {}
